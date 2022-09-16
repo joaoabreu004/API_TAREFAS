3c42269be189205f6f4e78adc492ab517902dea0
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-09-16 15:09:23 -0300

    Realizando o Deploy da Aplicação

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { AppController } from './app.controller';
+import { CategoriaModule } from './categoria/modules/categoria.module';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { TarefaModule } from './tarefa/modules/tarefa.module';
+import { Tarefa } from './tarefa/entities/tarefa.entity';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'MMJ210491',
+      database: 'db_todo_list_api',
+      entities: [Tarefa, Categoria],
+      synchronize: true
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres', 
+    //   url: process.env.DATABASE_URL, 
+    //   logging: false, 
+    //   dropSchema: false,
+    //   ssl: {
+    //     rejectUnauthorized: false
+    //   }, 
+    //   synchronize: true, 
+    //   autoLoadEntities: true
+    // }), 
+    TarefaModule,
+    CategoriaModule
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
